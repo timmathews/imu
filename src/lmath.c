aa868512cb66a946aa81af2a015d205f54adfad9
Tim Mathews <tim@signalk.org>
2015-09-22 18:56:39 +0000

    Updated Math Support
    Added new matrix and vector functions and made code more C99ish

--- src/lmath.c
+++ src/lmath.c
@@ -1,12 +1,12 @@
 #include "lmath.h"
 
-void matrix_multiply3(double a[3][3], double b[3][3], double m[3][3]) {
+void matrix_multiply3(double a[3][3], double b[3][3], double m[3][3])
+{
 	double op[3];
-	int i, j, k;
 
-	for(i=0; i<3; ++i) {
-		for(j=0; j<3; ++j) {
-			for(k=0; k<3; ++k) {
+	for(int i=0; i<3; ++i) {
+		for(int j=0; j<3; ++j) {
+			for(int k=0; k<3; ++k) {
 				op[k] = a[i][k] * b[k][j];
 			}
 
@@ -15,35 +15,57 @@ void matrix_multiply3(double a[3][3], double b[3][3], double m[3][3]) {
 	}
 }
 
-double vector_dot_product3(double a[3], double b[3]) {
-	double m = 0;
-	int i = 0;
+void matrix_add3(double a[3][3], double b[3][3], double m[3][3])
+{
+	for(int i=0; i<3; ++i) {
+		for(int j=0; j<3; ++j) {
+			m[i][j] = a[i][j] + b[i][j];
+		}
+	}
+}
 
-	for(; i<3; ++i) {
+double vector_dot_product3(double a[3], double b[3])
+{
+	double m = 0;
+
+	for(int i = 0; i<3; ++i) {
 		m+= a[i] * b[i];
 	}
 
 	return m;
 }
 
-void vector_cross_product3(double a[3], double b[3], double m[3]) {
+void vector_cross_product3(double a[3], double b[3], double m[3])
+{
 	m[0] = (a[1] * b[2]) - (a[2] * b[1]);
 	m[1] = (a[2] * b[0]) - (a[0] * b[2]);
 	m[2] = (a[0] * b[1]) - (a[1] * b[0]);
 }
 
-void vector_scale3(double a[3], double s, double m[3]) {
-	int i = 0;
-
-	for(; i<3; ++i) {
+void vector_scale3(double a[3], double s, double m[3])
+{
+	for(int i = 0; i<3; ++i) {
 		m[i] = a[i] * s;
 	}
 }
 
-void vector_add3(double a[3], double b[3], double m[3]) {
-	int i = 0;
-
-	for(; i<3; ++i) {
+void vector_add3(double a[3], double b[3], double m[3])
+{
+	for(int i = 0; i<3; ++i) {
 		m[i] = a[i] + b[i];
+	}
+}
+
+void vector_subtract3(double a[3], double b[3], double m[3])
+{
+	for(int i = 0; i<3; ++i) {
+		m[i] = a[i] - b[i];
+	}
+}
+
+void vector_multiply3(double a[3], double b[3], double m[3])
+{
+	for(int i = 0; i<3; ++i) {
+		m[i] = a[i] * b[i];
 	}
 }
--- src/lmath.h
+++ src/lmath.h
@@ -2,9 +2,12 @@
 #define _LMATH_H
 
 void matrix_multiply3(double a[3][3], double b[3][3], double m[3][3]);
+void matrix_add3(double a[3][3], double b[3][3], double m[3][3]);
 double vector_dot_product3(double v1[3], double v2[3]);
 void vector_cross_product3(double a[3], double b[3], double m[3]);
 void vector_scale3(double a[3], double s, double m[3]);
 void vector_add3(double a[3], double b[3], double m[3]);
+void vector_subtract3(double a[3], double b[3], double m[3]);
+void vector_multiply3(double a[3], double b[3], double m[3]);
 
 #endif
